7710799aaacdd845a55b3a5a366a2dc7c584b8aa
zannatulferdous0162 <zannatulferdous0162@gmail.com>
2024-03-01 11:16:09 +0600

    second commit

--- js/phone.js
+++ js/phone.js
@@ -1,85 +1,103 @@
-const loadPhone = async (searchText, isShowALL) => {
-  const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`);
-  const data = await res.json();
-  const phones = data.data
-  // console.log(phones);
-  displayPhone(phones, isShowALL)
-}
+const loadPhone = async (searchText, isShowAll) => {
+  try {
+    const response = await fetch(
+      `https://openapi.programming-hero.com/api/phones?search=${searchText}`
+    );
+    const data = await response.json();
+    const phones = data.data;
+    displayPhones(phones, isShowAll);
+  } catch (error) {
+    console.error("Error!");
+  }
+};
 
-const displayPhone = (phones, isShowALL) => {
-  // console.log(phones)
-  const phoneContainer = document.getElementById('phone-container');
-  // clear phone container cards before adding new card
-  phoneContainer.textContent = '';
-
-  //display show all button if there are more than 12 phones
-  const showAllContainer = document.getElementById('show-all-button');
-  if(phones.length > 12 && !isShowALL){
-    showAllContainer.classList.remove('hidden');
+const displayPhones = (phones, isShowAll) => {
+  const phoneContainer = document.getElementById("phoneContainer");
+  const showAllContainer = document.getElementById("showAllContainer");
+  phoneContainer.textContent = "";
+  if (phones.length > 12 && !isShowAll) {
+    showAllContainer.classList.remove("hidden");
+  } else {
+    showAllContainer.classList.add("hidden");
+  }
+  if (!isShowAll) {
+    phones = phones.slice(0, 12);
+  }
+  const noPhone = document.getElementById('noFoundMessage');
+  if(phones.length === 0){
+      noPhone.classList.remove('hidden');
   }
   else{
-    showAllContainer.classList.add('hidden');
+      noPhone.classList.add('hidden');
   }
-  console.log('Is Show All',isShowALL);
+  phones.forEach((phone) => {
+    const div = document.createElement("div");
+    div.classList.add(
+      "p-2",
+      "rounded",
+      "border",
+      "border-white",
+      "bg-white",
+      "shadow"
+    );
+    div.innerHTML = `
+    <img src="${phone.image}" alt="Image" class="rounded mx-auto">
+    <div class="text-center">
+        <h2 class="text-lg text-black/80 font-bold my-2">${phone.phone_name}</h2>
+        <p class="text-green-600 text-base mb-2">${phone.brand}</p>
+        <button onclick="handleShowDetail('${phone.slug}')" class="p-2 bg-black text-white font-semibold rounded text-lg">
+            Button
+        </button>
+    </div>
+    `;
+    phoneContainer.appendChild(div);
+  });
+  loadingToggle(false);
+};
 
-  // display only first 12 phones if not show all
-  if(!isShowALL){
-    phones = phones.slice(0,12);
+const handleShowDetail = async (id) => {
+  const response = await fetch(
+    `https://openapi.programming-hero.com/api/phone/${id}`
+  );
+  const data = await response.json();
+  const phone = data.data;
+  showPhoneDetail(phone);
+};
+
+const showPhoneDetail = (phone) => {
+  const phoneDetailsContainer = document.getElementById(
+    "phoneDetailsContainer"
+  );
+  phoneDetailsContainer.innerHTML = `
+  <img src="${phone.image}" alt="Image" class="mx-auto rounded">
+  <h3 class="text-center text-white font-bold my-2 text-xl">${phone?.name}</h3>
+  <p class="text-center text-white font-bold my-2 text-base">${phone?.mainFeatures?.storage}</p>
+  <p class="text-center text-white font-bold my-2 text-base">${phone?.mainFeatures?.displaySize}</p>
+  <p class="text-center text-white font-bold my-2 text-base">${phone?.mainFeatures?.memory}</p>
+  <p class="text-center text-white font-bold my-2 text-base">${phone?.mainFeatures?.sensors}</p>
+  <p class="text-center text-white font-bold my-2 text-base">${phone?.releaseDate}</p>
+  <p class="text-center text-white font-bold my-2 text-base">${phone?.brand}</p>
+  <p class="text-center text-white font-bold my-2 text-base">${phone?.others?.GPS || 'No GPS'}</p>
+  `;
+  show_details_modal.showModal();
+};
+
+const handleSearch = (isShowAll) => {
+  loadingToggle(true);
+  const searchInput = document.getElementById("searchInput");
+  const searchInputValue = searchInput.value;
+  loadPhone(searchInputValue, isShowAll);
+};
+
+const loadingToggle = (isLoading) => {
+  const loader = document.getElementById("loader");
+  if (isLoading) {
+    loader.classList.remove("hidden");
+  } else {
+    loader.classList.add("hidden");
   }
-
-  phones.forEach(phone => {
-    console.log(phone)
-    // 2--> create a div
-    const phoneCard = document.createElement('div');
-    phoneCard.classList = `card p-4 rounded-[10px] bg-gray-100 shadow-xl`;
-
-    //3--> set inner html
-    phoneCard.innerHTML = ` 
-        <figure><img src="${phone.image}" alt="Shoes" /></figure>
-        <div class="card-body">
-          <h2 class="card-title">${phone.phone_name}</h2>
-          <p>If a dog chews shoes whose shoes does he choose?</p>
-          <div class="card-actions justify-center">
-            <button class="btn btn-primary text-white font-bold">Show Details</button>
-          </div>
-        </div>`;
-    // 4 --> append child
-    phoneContainer.appendChild(phoneCard);
-
-  })
-  //hide loading spinner
-  toggolLoadingSpinner(false);
-}
-
-const handelSearch = (isShowALL) => {
-  toggolLoadingSpinner(true);
-  const searchField = document.getElementById('search-feild');
-  const serchText = searchField.value;
-  // console.log(serchText)
-  loadPhone(serchText,isShowALL);
-}
-
-// const handelSearch2 = () => {
-//   toggolLoadingSpinner(true);
-//   const searchField = document.getElementById('search-feild2');
-//   const searchText = searchField.value;
-//   loadPhone(searchText);
-// }
-
-// loadding or spinner
-const toggolLoadingSpinner = (isLoading) => {
-  const loddingSpinner = document.getElementById('loading-spinner');
-  if(isLoading){
-    loddingSpinner.classList.remove('hidden');
-  }
-  else{
-    loddingSpinner.classList.add('hidden');
-  }
-}
+};
 
 const handleShowAll = () => {
-  handelSearch(true);
-
-}
-
-// loadPhone();
+  handleSearch(true);
+};
